cc1ff2bbd51d14655dcb23da7ada47dca4734d00
svalenciag95 <svalenciag95@gmail.com>
2023-10-04 17:14:25 -0500

    Conexion index con js

--- InterfazGraficadeacceso/src/js/app.js
+++ InterfazGraficadeacceso/src/js/app.js
@@ -1,22 +1,16 @@
-function guardarDatos() {
-    var nombreCliente = document.getElementById('nombreCliente').value;
-    var tipoMoto = document.getElementById('tipoMoto').value;
-    var placaMoto = document.getElementById('placaMoto').value;
+function guardarDatos(event) {
+    event.preventDefault();
 
-    // Envía los datos al servidor
-    var xhr = new XMLHttpRequest();
-    xhr.open("POST", "guardar_datos.php", true);
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        
-    xhr.onreadystatechange = function () {
+    var nombreCliente = document.getElementById('nombre').value;
+    var marcaMoto = document.getElementById('marca').value;
+    var cilindraje = document.getElementById('cilindraje').value;
+    var placaMoto = document.getElementById('placa').value.toUpperCase(); 
+    var tipoLavado = document.getElementById('tipoLavado').value;
 
-        if (xhr.readyState === 4 && xhr.status === 200) {
-                // Respuesta del servidor (puedes manejarla según tus necesidades)
-                console.log(xhr.responseText);
-            }
-        };
-        
-    var params = "nombreCliente=" + nombreCliente + "&tipoMoto=" + tipoMoto + "&placaMoto=" + placaMoto;
-    xhr.send(params);
-    }
-
+    // Puedes hacer lo que necesites con los datos, por ejemplo, enviarlos a un servidor
+    console.log("Nombre del Cliente:", nombreCliente);
+    console.log("Marca de la Moto:", marcaMoto);
+    console.log("Cilindraje:", cilindraje);
+    console.log("Placa de la Moto:", placaMoto);
+    console.log("Tipo de Lavado:", tipoLavado);
+}
